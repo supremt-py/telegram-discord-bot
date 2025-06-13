b799bda4ff2ed61551cf0af94cdc801820653152
supremt-py <smustafatatar@gmail.com>
2025-06-14 00:37:46 +0300

    Update discord_runner.py

--- discord_runner.py
+++ discord_runner.py
@@ -1,4 +1,17 @@
-from discord_bot import run_discord_bot
+import os
+import discord
+import asyncio
 
-# Discord botu tek başına başlatılıyor
-run_discord_bot()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+client = discord.Client(intents=discord.Intents.default())
+
+async def send_to_discord(text):
+    await client.wait_until_ready()
+    channel = client.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send(text)
+
+def run_discord_bot():
+    asyncio.run(client.start(DISCORD_TOKEN))
