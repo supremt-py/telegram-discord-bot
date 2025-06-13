97d8fc6d44bcddfbff17a412a3524223e4c5e42f
supremt-py <smustafatatar@gmail.com>
2025-06-14 00:35:08 +0300

    Create entrypoint.py

---
+++ entrypoint.py
@@ -0,0 +1,9 @@
+import threading
+from telegram_bot import start_telegram_bot
+from discord_runner import run_discord_bot
+
+# Discord botunu ayrı bir thread'de başlat
+threading.Thread(target=run_discord_bot).start()
+
+# Telegram botu ana thread'de çalışır
+start_telegram_bot()
