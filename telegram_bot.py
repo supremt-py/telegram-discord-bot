c9eb7e897583eaacf2b64d26d674450bed782266
supremt-py <smustafatatar@gmail.com>
2025-06-14 15:11:44 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,26 +1,18 @@
 import os
 import asyncio
 from telegram.ext import Updater, MessageHandler, Filters
-from discord_runner import send_to_discord, run_discord_bot
-import threading
+from discord_runner import send_to_discord
 
 def forward_channel_post(update, context):
     if update.channel_post:
         text = update.channel_post.text or update.channel_post.caption or "(Medyalı mesaj)"
         print("Telegram mesajı:", text)
-        loop = asyncio.get_event_loop()
-        loop.create_task(send_to_discord(text))
+        asyncio.run(send_to_discord(text))
 
-def start_telogram_bot():
+def start_telegram_bot():
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
     updater = Updater(TELEGRAM_TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(Filters.all, forward_channel_post))
     updater.start_polling()
     updater.idle()
-
-# Discord botu başlatılır
-threading.Thread(target=run_discord_bot).start()
-
-# Telegram botu başlatılır
-start_telogram_bot()
