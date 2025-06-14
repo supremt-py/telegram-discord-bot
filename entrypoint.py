a86ca9335e45888eb68a116793077ca47d04a4f9
supremt-py <smustafatatar@gmail.com>
2025-06-14 19:40:10 +0300

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,10 +1,7 @@
 from telegram_bot import start_telegram_bot
 from discord_runner import bot
 import threading
-import os  # ← eksik olan bu satırdı
+import os
 
-# Discord botunu ayrı thread'de başlat
 threading.Thread(target=bot.run, args=(os.getenv("DISCORD_TOKEN"),)).start()
-
-# Telegram botunu başlat
 start_telegram_bot()
