4741fa9bf6da8122b7612812ec07fc06a75e4925
supremt-py <smustafatatar@gmail.com>
2025-06-14 16:46:06 +0300

    Update discord_runner.py

--- discord_runner.py
+++ discord_runner.py
@@ -2,15 +2,14 @@ import os
 import discord
 import asyncio
 from discord.ext import commands
-from googletrans import Translator
 from langdetect import detect
+from deep_translator import GoogleTranslator
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 DISCORD_CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
-translator = Translator()
 telegram_to_discord = {}
 
 async def send_to_discord(message, telegram_msg_id=None):
@@ -18,17 +17,16 @@ async def send_to_discord(message, telegram_msg_id=None):
     channel = bot.get_channel(DISCORD_CHANNEL_ID)
 
     try:
-        # Türkçe olup olmadığını kontrol et
+        # Türkçe ise İngilizceye çevir
         lang = detect(message)
-        if lang == 'tr':
-            translated = translator.translate(message, src='tr', dest='en').text
+        if lang == "tr":
+            translated = GoogleTranslator(source="tr", target="en").translate(message)
             text_to_send = f"{message}\n\n**Translation:** {translated}"
         else:
             text_to_send = message
 
         discord_message = await channel.send(text_to_send)
 
-        # Telegram mesajı ID'si ile eşleştir
         if telegram_msg_id:
             telegram_to_discord[telegram_msg_id] = discord_message.id
 
