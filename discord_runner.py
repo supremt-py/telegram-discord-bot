64cbad9f3f118fa19e4e7bdc4dad588d5dca876f
supremt-py <smustafatatar@gmail.com>
2025-06-14 22:02:10 +0300

    Update discord_runner.py

--- discord_runner.py
+++ discord_runner.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
+intents.messages = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 telegram_to_discord = {}
@@ -13,6 +14,7 @@ async def on_ready():
 
 async def send_to_discord(text, telegram_msg_id=None):
     await bot.wait_until_ready()
+
     channel_id = os.getenv("DISCORD_CHANNEL_ID")
     if not channel_id:
         print("HATA: DISCORD_CHANNEL_ID tanımlı değil!")
@@ -31,5 +33,5 @@ async def send_to_discord(text, telegram_msg_id=None):
     except Exception as e:
         print(f"Discord'a mesaj gönderilirken hata oluştu: {e}")
 
-def run_discord_bot():
-    bot.run(os.getenv("DISCORD_TOKEN"))
+async def start_discord_bot():
+    await bot.start(os.getenv("DISCORD_TOKEN"))
