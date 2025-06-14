439520698b00c37a0721c78bd9976aea00e124f7
supremt-py <smustafatatar@gmail.com>
2025-06-14 23:36:41 +0300

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,10 +1,10 @@
-import asyncio
 from telegram_bot import start_telegram_bot
-from discord_runner import start_discord_bot
+from discord_runner import bot
+import threading
+import os
 
-async def main():
-    asyncio.create_task(start_discord_bot())
-    await start_telegram_bot()
+# Discord botunu ayrı thread’de başlat
+threading.Thread(target=bot.run, args=(os.getenv("DISCORD_TOKEN"),)).start()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+# Telegram botunu ana akışta başlat
+start_telegram_bot()
