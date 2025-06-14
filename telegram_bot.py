355a409f506b400de1be5a4321f96d2dea738b9e
supremt-py <smustafatatar@gmail.com>
2025-06-14 20:32:58 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 from discord_runner import send_to_discord
@@ -7,7 +8,8 @@ async def forward_channel_post(update: Update, context: ContextTypes.DEFAULT_TYP
     if update.channel_post:
         text = update.channel_post.text or update.channel_post.caption or "(Medyalı mesaj)"
         print("Telegram mesajı:", text)
-        await send_to_discord(text)
+        # await yerine asyncio.create_task kullandık
+        asyncio.create_task(send_to_discord(text))
 
 def start_telegram_bot():
     print("Telegram bot başlatılıyor...")
