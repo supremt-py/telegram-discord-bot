a6ec224e80d87aac245c2aac08637883eebdf9e5
supremt-py <smustafatatar@gmail.com>
2025-06-14 20:34:31 +0300

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,7 +1,14 @@
-from telegram_bot import start_telegram_bot
-from discord_runner import bot
 import threading
 import os
+from telegram_bot import start_telegram_bot
+from discord_runner import bot
 
-threading.Thread(target=bot.run, args=(os.getenv("DISCORD_TOKEN"),)).start()
+# Discord botunu ayrı bir thread'de başlat
+discord_token = os.getenv("DISCORD_TOKEN")
+if not discord_token:
+    print("HATA: DISCORD_TOKEN tanımlı değil!")
+else:
+    threading.Thread(target=bot.run, args=(discord_token,)).start()
+
+# Telegram botunu başlat
 start_telegram_bot()
