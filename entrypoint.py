c4b8ff5f68faa3d372a7b8d3d06b0356eaf18589
supremt-py <smustafatatar@gmail.com>
2025-06-14 23:06:53 +0300

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -3,10 +3,7 @@ from telegram_bot import start_telegram_bot
 from discord_runner import start_discord_bot
 
 async def main():
-    # Discord botu arka planda başlasın
     asyncio.create_task(start_discord_bot())
-
-    # Telegram botu aynı event loop içinde
     await start_telegram_bot()
 
 if __name__ == "__main__":
