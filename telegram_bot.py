d597f0aecf944f68c1bd76df890afc212744d9e1
supremt-py <smustafatatar@gmail.com>
2025-06-14 16:41:40 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,13 +10,24 @@ from telegram.ext import (
     EditedMessageHandler
 )
 from discord_runner import send_to_discord, telegram_to_discord, bot
+from langdetect import detect
+
+
+def is_only_turkish(text):
+    try:
+        return detect(text) == 'tr'
+    except:
+        return False
+
 
 async def forward_channel_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.channel_post:
         text = update.channel_post.text or update.channel_post.caption or "(Medyalı mesaj)"
         msg_id = update.channel_post.message_id
         print("Telegram mesajı:", text)
-        await send_to_discord(text, telegram_msg_id=msg_id)
+        if is_only_turkish(text):
+            await send_to_discord(text, telegram_msg_id=msg_id)
+
 
 async def handle_deleted_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg_id = update.channel_post.message_id
@@ -29,18 +40,21 @@ async def handle_deleted_message(update: Update, context: ContextTypes.DEFAULT_T
         except Exception as e:
             print(f"Silme hatası: {e}")
 
+
 async def handle_edited_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.edited_channel_post.text or update.edited_channel_post.caption or "(Medyalı düzenleme)"
     msg_id = update.edited_channel_post.message_id
     print("Telegram düzenleme:", text)
-    discord_msg_id = telegram_to_discord.get(msg_id)
-    if discord_msg_id:
-        channel = bot.get_channel(int(os.getenv("DISCORD_CHANNEL_ID")))
-        try:
-            msg = await channel.fetch_message(discord_msg_id)
-            await msg.edit(content=text)
-        except Exception as e:
-            print(f"Güncelleme hatası: {e}")
+    if is_only_turkish(text):
+        discord_msg_id = telegram_to_discord.get(msg_id)
+        if discord_msg_id:
+            channel = bot.get_channel(int(os.getenv("DISCORD_CHANNEL_ID")))
+            try:
+                msg = await channel.fetch_message(discord_msg_id)
+                await msg.edit(content=text)
+            except Exception as e:
+                print(f"Güncelleme hatası: {e}")
+
 
 def start_telegram_bot():
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
