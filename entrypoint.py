2bdb31e643074c9bcabb8c25ac4c2c99dd50f326
supremt-py <smustafatatar@gmail.com>
2025-06-14 16:37:02 +0300

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,6 @@
 from telegram_bot import start_telegram_bot
-from discord_runner import start_discord_bot
+from discord_runner import run_discord_bot
 import threading
 
-threading.Thread(target=start_discord_bot).start()
+threading.Thread(target=run_discord_bot).start()
 start_telegram_bot()
