3b8f009ce736a408b8fe69b3ef99dcb59fe5df8a
supremt-py <smustafatatar@gmail.com>
2025-06-14 23:36:02 +0300

    Update discord_runner.py

--- discord_runner.py
+++ discord_runner.py
@@ -22,15 +22,15 @@ async def send_to_discord(text, telegram_msg_id=None):
 
     try:
         channel = bot.get_channel(int(channel_id))
+
         if not channel:
             print("Hedef Discord kanalı bulunamadı.")
             return
 
         msg = await channel.send(text)
+
         if telegram_msg_id:
             telegram_to_discord[telegram_msg_id] = msg.id
+
     except Exception as e:
         print(f"Discord'a mesaj gönderilirken hata oluştu: {e}")
-
-async def start_discord_bot():
-    await bot.start(os.getenv("DISCORD_TOKEN"))
