4bb69afad45cf29d63ebe94fbafd290f475a0905
supremt-py <smustafatatar@gmail.com>
2025-06-14 17:27:27 +0300

    Update discord_runner.py

--- discord_runner.py
+++ discord_runner.py
@@ -2,11 +2,9 @@ import os
 import discord
 from discord.ext import commands
 
-# Discord bot için gerekli izinleri ayarla
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Telegram mesaj ID'si ile Discord mesaj ID'si eşleştirmesi
 telegram_to_discord = {}
 
 @bot.event
@@ -15,11 +13,21 @@ async def on_ready():
 
 async def send_to_discord(text, telegram_msg_id=None):
     await bot.wait_until_ready()
-    channel_id = int(os.getenv("DISCORD_CHANNEL_ID"))
-    channel = bot.get_channel(channel_id)
-    if not channel:
-        print("Hedef Discord kanalı bulunamadı.")
+
+    channel_id = os.getenv("DISCORD_CHANNEL_ID")
+    if not channel_id:
+        print("Hata: DISCORD_CHANNEL_ID ortam değişkeni tanımlı değil.")
         return
-    msg = await channel.send(text)
-    if telegram_msg_id:
-        telegram_to_discord[telegram_msg_id] = msg.id
+
+    try:
+        channel_id = int(channel_id)
+        channel = bot.get_channel(channel_id)
+        if not channel:
+            print("Hedef Discord kanalı bulunamadı.")
+            return
+
+        msg = await channel.send(text)
+        if telegram_msg_id:
+            telegram_to_discord[telegram_msg_id] = msg.id
+    except Exception as e:
+        print(f"Discord'a mesaj gönderilirken hata oluştu: {e}")
