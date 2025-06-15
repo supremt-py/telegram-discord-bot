c2a5cd3b01c668ec225623744a8ca8a7bb1aee62
supremt-py <smustafatatar@gmail.com>
2025-06-15 12:30:06 +0300

    Update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -5,6 +5,7 @@ from telegram_bot import start_telegram_bot
 from discord_runner import start_discord_bot
 
 async def main():
+    print("Entrypoint başladı!")  # Bu satırı en üstteki `main()` fonksiyonunun hemen başına ekle
     discord_task = asyncio.create_task(start_discord_bot())
     telegram_task = asyncio.create_task(start_telegram_bot())
     await asyncio.gather(discord_task, telegram_task)
